8e57e140e217bfd03ef3d2114319e8e0cdea1bd5
Joshua <88637697+JoshuaNope@users.noreply.github.com>
2022-02-20 16:09:52 +0700

    FLUTTER_APPLICATION_1

---
+++ Flutter_1/lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue[700],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage(
+                  'images/logo_fti.png'
+                ),
+              ),
+              Divider(
+                height: 30,
+              ),
+              Text(
+                'Hello World',
+                style: TextStyle(
+                  color: Colors.blue[700],
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
